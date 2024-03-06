6f81ee138a32f72c2c6c903e2eb3e1ac9b7b46c6
Ksymena P <ksymenka@duck.com>
2024-03-06 12:27:13 +0100

    updated fish.config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
   fastfetch --config /home/ksymena/.config/fastfetch/start.jsonc
   set fish_greeting ""
+  export EDITOR=nvim
+  export VISUAL=nvim
+  gh auth setup-git 
   echo ""
 end
