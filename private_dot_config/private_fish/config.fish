ef6ab0f3d85a9729f9411f16f0607890d6a42a3a
Ksymena P <ksymenka@duck.com>
2024-03-26 19:15:05 +0100

    changed pager to bat

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
   fastfetch --config /home/ksymena/.config/fastfetch/start.jsonc
   set fish_greeting ""
+  export PAGER=bat
   export EDITOR=nvim
   export VISUAL=nvim
   gh auth setup-git 
