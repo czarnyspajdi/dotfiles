07514cbd13a90418230be99d7de0fccc0aba2a32
Szymon P <szymon_jozef@proton.me>
2024-09-10 18:09:58 +0200

    added atuin

--- private_dot_config/fish/executable_config.fish
+++ private_dot_config/fish/executable_config.fish
@@ -1,9 +1,10 @@
 if status is-interactive
-  fastfetch --config $HOME/.config/fastfetch/start.jsonc
-  set fish_greeting ""
-  export PAGER=bat
-  export EDITOR=nvim
-  export VISUAL=nvim
-  gh auth setup-git 
-  echo ""
+    fastfetch --config $HOME/.config/fastfetch/start.jsonc
+    set fish_greeting ""
+    export PAGER=bat
+    export EDITOR=nvim
+    export VISUAL=nvim
+    gh auth setup-git
+    eval "$(atuin init fish)"
+    echo ""
 end
