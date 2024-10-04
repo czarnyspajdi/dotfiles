8cd1bf266b3948b75b9f3cf1b921b2a414a58802
Szymon P <szymon_jozef@proton.me>
2024-10-04 18:30:06 +0200

    HAHAHA

--- private_dot_config/waybar/scripts/executable_makoTurn.sh
+++ private_dot_config/waybar/scripts/executable_makoTurn.sh
@@ -2,9 +2,11 @@
 
 # Sprawdzamy, czy Mako jest uruchomiony
 if pgrep -x "mako" >/dev/null; then
-  pkill mako
-  echo '{"icon": " "}' # Ikona przekreślonego dzwonka
+  pkill -f "mako" # Zamykamy Mako
+  sleep 0.2       # Dajemy trochę czasu, żeby Mako faktycznie się wyłączył
+  echo '{" "}'   # Ikona przekreślonego dzwonka
 else
   mako &
-  echo '{"icon": " "}' # Ikona normalnego dzwonka
+  disown        # Uruchamiamy Mako w tle i odłączamy od terminala
+  echo '{" "}' # Ikona normalnego dzwonka
 fi
