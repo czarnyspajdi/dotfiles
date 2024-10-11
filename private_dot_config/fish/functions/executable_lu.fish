b863315f9987c852599725886ed10044d796eac7
Ksymena P <ksymenka@duck.com>
2024-10-11 16:01:26 +0200

    Remove .config/fish/functions/lu.fish Remove .config/fish/functions/osync.fish

--- private_dot_config/fish/functions/executable_lu.fish
+++
@@ -1,4 +0,0 @@
-function lu --wraps='sudo pacman -Sy && pacman -Qu' --description 'alias lu=sudo pacman -Sy && pacman -Qu'
-  sudo pacman -Sy && pacman -Qu $argv
-        
-end
--- private_dot_config/fish/functions/executable_osync.fish
+++
@@ -1,4 +0,0 @@
-function osync --wraps=echo\ \"gdrive\"\ \&\&\ rclone\ sync\ \~/Obrazy/fajne\\\ obrazki/\ gdrive:obrazy/fajne\\\ obrazki/\ \&\&\ echo\ \"nextcloud\"\ \&\&\ rclone\ sync\ \~/Obrazy/fajne\\\ obrazki/\ /mnt/uwu/nextcloud/pliki/obrazy/najfajniejsze\\\ obrazki/fajne\\\ obrazki/ --description alias\ osync=echo\ \"gdrive\"\ \&\&\ rclone\ sync\ \~/Obrazy/fajne\\\ obrazki/\ gdrive:obrazy/fajne\\\ obrazki/\ \&\&\ echo\ \"nextcloud\"\ \&\&\ rclone\ sync\ \~/Obrazy/fajne\\\ obrazki/\ /mnt/uwu/nextcloud/pliki/obrazy/najfajniejsze\\\ obrazki/fajne\\\ obrazki/
-  echo "gdrive" && rclone sync ~/Obrazy/fajne\ obrazki/ gdrive:obrazy/fajne\ obrazki/ && echo "nextcloud" && rclone sync ~/Obrazy/fajne\ obrazki/ /mnt/uwu/nextcloud/pliki/obrazy/najfajniejsze\ obrazki/fajne\ obrazki/ $argv
-        
-end
