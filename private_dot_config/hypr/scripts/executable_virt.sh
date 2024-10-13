82a7309a915a8ef5d0d0d6d3cbee9810a789cddb
Szymon P <szymon_jozef@proton.me>
2024-10-13 12:16:18 +0200

    Update .config/hypr/scripts/virt.sh Update .config/vesktop/settings.json

--- private_dot_config/hypr/scripts/executable_virt.sh
+++ private_dot_config/hypr/scripts/executable_virt.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
+
 ISO_PATH="$HOME/.qemu/windows10.iso"
 DISK_PATH="$HOME/.qemu/windows10.qcow2"
 DISK_SIZE="60G"
 MEMORY="6G"
 CPU="4"
+VM_NAME="windows10"
+
+# Funkcja do wyłączania wszystkich uruchomionych maszyn wirtualnych
+shutdown_vms() {
+  echo "Wyłączam wszystkie uruchomione maszyny wirtualne..."
+  # Znajdowanie i zabijanie wszystkich procesów qemu-system-x86_64
+  pgrep -f qemu-system-x86_64 | xargs -r kill
+  exit 0 # Kończymy działanie skryptu
+}
+
+# Sprawdzenie, czy jakakolwiek maszyna wirtualna jest uruchomiona
+if pgrep -f qemu-system-x86_64 >/dev/null; then
+  shutdown_vms
+fi
 
 # Sprawdzenie, czy VM o nazwie windows10 już istnieje
 if [ -f "$DISK_PATH" ]; then
@@ -14,13 +30,13 @@ if [ -f "$DISK_PATH" ]; then
     -m "$MEMORY" \
     -smp "$CPU" \
     -hda "$DISK_PATH" \
-    -device virtio-net,netdev=net0 \
-    -netdev user,id=net0,hostfwd=tcp::2222-:22 \
     -vga virtio \
     -boot order=c \
+    -usb -device usb-tablet \
+    -display sdl,gl=on \
     -enable-kvm \
     -cpu host \
-    -display default,show-cursor=on \
+    -display default,show-cursor=off \
     -device ich9-intel-hda -device hda-duplex
 
 else
@@ -31,8 +47,9 @@ else
 
   # Sprawdzenie, czy ISO istnieje
   if [ ! -f "$ISO_PATH" ]; then
-    echo "Brak pliku ISO. Pobierz obraz Windows 10 i umieść w $ISO_PATH."
-    exit 1
+    echo "Brak pliku ISO. Otwieram stronę do pobrania Windows 10 w przeglądarce."
+    xdg-open "https://www.microsoft.com/pl-pl/software-download/windows10" # Otwiera stronę pobierania
+    exit 1                                                                 # Kończymy działanie skryptu, gdy ISO nie istnieje
   else
     echo "ISO Windows 10 już istnieje."
   fi
