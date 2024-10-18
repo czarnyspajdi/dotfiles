1e9b0cdbb8b14c90b6bae23a90fa958bb4d5a51c
Szymon P <szymon_jozef@proton.me>
2024-10-18 17:43:26 +0200

    hehe

--- run_setup_vars.sh
+++ run_setup_vars.sh
@@ -2,26 +2,30 @@
 
 cpu_cores=$(lscpu --json | jq -r '.lscpu[] | select(.field == "CPU:") | .data')
 threads_per_core=$(lscpu --json | jq -r '.lscpu[] | select(.field == "Wątków na rdzeń:") | .data')
+cpu_threads=$((cpu_cores * threads_per_core))
 
-cpu=$((cpu_cores * threads_per_core / 2))
 ram=$(lsmem --json | jq -r '.memory[] | .size' | sed 's/,/./g' | sed 's/G//' | awk '{s+=$1} END {print s}')
 
-echo "Total number of CPU threads: $((cpu_cores * threads_per_core))"
-echo "Half of the cpu threads: $cpu"
+half_cpu_threads=$(echo "($cpu_threads + 1) / 2" | bc)
+half_ram=$(echo "($ram + 1) / 2" | bc)
+
+echo "Total number of CPU threads: $cpu_threads"
+echo "Half of the CPU threads: $half_cpu_threads"
 echo "Total RAM size: $ram GB"
-echo "Half of the RAM: $((ram / 2)) GB"
-echo -e "\e[33mIMPORTANT: Half of the values will be used for vm settings. To change this edit this script OR file at $HOME/.config/chezmoi/chezmoi.toml\e[0m"
+echo "Half of the RAM: $half_ram GB"
 
-# setup chezmoi variable like this:
+echo -e "\e[33mIMPORTANT: Half of the values will be used for VM settings. To change this edit this script OR file at $HOME/.config/chezmoi/chezmoi.toml\e[0m"
 
-vm_hardware="[data.vm.hardware]\n
-\tcpu = $cpu\n 
-\tram = $((ram / 2))\n"
+# add your chezmoi variables here
 
-git="[git]\n
-\tautoCommit = true\n
+vm_hardware="[data.vm.hardware]\n\
+\tcpu = $half_cpu_threads\n\
+\tram = $half_ram\n"
+
+git="[git]\n\
+\tautoCommit = true\n\
 \tautoPush = true\n"
 
-file_content="$vm_hardware$git" # also add it here
+file_content="$vm_hardware$git" # and add it here
 
-echo -e $file_content >$HOME/.config/chezmoi/chezmoi.toml
+echo -e "$file_content" >"$HOME/.config/chezmoi/chezmoi.toml"
