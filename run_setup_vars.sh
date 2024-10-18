38606745183d67e963d998e09b3ee604a4a068c5
Szymon P <szymon_jozef@proton.me>
2024-10-18 17:28:48 +0200

    hehe

--- run_setup_vars.sh
+++ run_setup_vars.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-# Pobierz liczbę rdzeni procesora i wątków na rdzeń
 
 cpu_cores=$(lscpu --json | jq -r '.lscpu[] | select(.field == "CPU:") | .data')
 threads_per_core=$(lscpu --json | jq -r '.lscpu[] | select(.field == "Wątków na rdzeń:") | .data')
 
-# Oblicz liczbę wątków i połowę wątków
 cpu=$((cpu_cores * threads_per_core / 2))
-
-# Pobierz sumę pamięci RAM
 ram=$(lsmem --json | jq -r '.memory[] | .size' | sed 's/,/./g' | sed 's/G//' | awk '{s+=$1} END {print s}')
 
-# Wyświetl wyniki
-echo "Liczba wszystkich wątków procesora: $((cpu_cores * threads_per_core))"
-echo "Połowa wątków procesora: $cpu"
-echo "Suma pamięci RAM: $ram GB"
-echo "Połowa pamięci RAM: $((ram / 2)) GB"
+echo "Total number of CPU threads: $((cpu_cores * threads_per_core))"
+echo "Half of the cpu threads: $cpu"
+echo "Total RAM size: $ram GB"
+echo "Half of the RAM: $((ram / 2)) GB"
+echo -e "\e[33mIMPORTANT: Half of the values will be used for vm settings. To change this edit this script OR file at $HOME/.config/chezmoi/chezmoi.toml\e[0m"
 
-# Twórz zawartość pliku
-file_content="[data.vm.hardware]\n
+# setup chezmoi variable like this:
+
+vm_hardware="[data.vm.hardware]\n
 \tcpu = $cpu\n 
-\tram = $((ram / 2))"
+\tram = $((ram / 2))\n"
 
-echo -e $file_content >>$HOME/.config/chezmoi/chezmoi.toml
+git="[git]\n
+\tautoCommit = true\n
+\tautoPush = true\n"
+
+file_content="$vm_hardware$git" # also add it here
+
+echo -e $file_content >$HOME/.config/chezmoi/chezmoi.toml
