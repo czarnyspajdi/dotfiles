18daa71728beffb4c060d2a95d4be5e2c9e0ebc0
Szymon P <szymon_jozef@proton.me>
2024-10-22 19:12:51 +0200

    Update .config/fish/functions/update.fish

--- private_dot_config/fish/functions/executable_update.fish
+++ private_dot_config/fish/functions/executable_update.fish
@@ -1,4 +1,4 @@
 function update --description 'alias update=sudo pacman -Syu && flatpak update'
-    sudo pacman -Syu && flatpak update && chezmoi update $argv
+    sudo pacman -Syu && flatpak update --noninteractive && chezmoi update $argv
 
 end
