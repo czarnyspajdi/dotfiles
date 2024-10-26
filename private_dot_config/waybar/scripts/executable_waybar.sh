ee48cbc8d1b122b36d04721d2d985a1786f0ed59
Szymon P <szymon_jozef@proton.me>
2024-10-26 19:51:45 +0200

    Update .config/waybar/scripts/waybar.sh

--- private_dot_config/waybar/scripts/executable_waybar.sh
+++ private_dot_config/waybar/scripts/executable_waybar.sh
@@ -2,6 +2,8 @@
 
 # Terminate already running bar instances
 killall -q waybar
+killall -q mediaplayer.sh
+killall -q playerctl
 
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
