bc28ba3017b001e86eb48d80dc914d0b0809b4a2
Szymon P <szymon_jozef@proton.me>
2024-12-11 12:15:06 +0100

    Update .config/waybar/config.jsonc Update .local/bin/mute_notifications.sh

--- private_dot_local/bin/executable_mute_notifications.sh
+++ private_dot_local/bin/executable_mute_notifications.sh
@@ -5,15 +5,16 @@ is_muted=$(makoctl mode 2>/dev/null | grep -vi 'default' | grep -qi 'disturb' &&
 if [[ $1 == "toggle" ]]; then
     if [[ $is_muted -eq 0 ]]; then
         makoctl mode -a do-not-disturb >/dev/null 2>&1
-        echo " "
+        echo " "
     else
         makoctl mode -r do-not-disturb >/dev/null 2>&1
-        echo ""
+        echo " "
     fi
 else
     if [[ $is_muted -eq 0 ]]; then
-        echo ""
+        echo " "
     else
         echo " "
     fi
 fi
+
